1b4084233f8c6226afaaeb5e0bf132a350232943
Maik Grunicke <h.m.grunicke@gmail.com>
2022-03-16 18:29:56 +0100

    Created Execute.java and KeyHandler.java

---
+++ Execute.java
@@ -0,0 +1,39 @@
+import com.github.kwhat.jnativehook.GlobalScreen;
+import com.github.kwhat.jnativehook.NativeHookException;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.InputEvent;
+
+public class Execute {
+    static public boolean toggleClicker;
+    static public boolean endClicker = true;
+    public static void main(String args[]){
+        JFrame mainFrame = new JFrame("My Autoclicker");
+        JPanel mainPanel = new JPanel(new FlowLayout());
+        mainFrame.getContentPane().add(mainPanel);
+        mainFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        mainFrame.setSize(300,200);
+        mainFrame.setVisible(true);
+        try {
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        }
+        GlobalScreen.addNativeKeyListener(new KeyHandler());
+        while (endClicker){
+            try{
+                Robot clicker = new Robot();
+                int button = InputEvent.BUTTON1_DOWN_MASK;
+                while(toggleClicker){
+                    clicker.mousePress(button);
+                    Thread.sleep(10);
+                    clicker.mouseRelease(button);
+                    Thread.sleep(10);
+                }
+            }catch(Exception e){
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ KeyHandler.java
@@ -0,0 +1,16 @@
+import com.github.kwhat.jnativehook.NativeInputEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyEvent;
+import com.github.kwhat.jnativehook.keyboard.NativeKeyListener;
+
+
+public class KeyHandler implements NativeKeyListener{
+    @Override
+    public void nativeKeyPressed(NativeKeyEvent e){
+        if(e.getKeyCode() == NativeKeyEvent.VC_SPACE){
+            Execute.toggleClicker = !Execute.toggleClicker;
+        }
+        if(e.getKeyCode() == NativeKeyEvent.VC_PERIOD){
+            Execute.endClicker = false;
+        }
+    }
+}
